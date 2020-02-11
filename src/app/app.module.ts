0abd7b0b3076bbba83e827daf2334eda6908b7c7
VictorKanama <victor_kanama@hotmail.com>
2020-02-11 15:34:12 +0200

    Angular Routing Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,16 +12,32 @@ import { AppComponent } from "./app.component";
 import "hammerjs";
 import { MenuComponent } from "./menu/menu.component";
 import { DishdetailComponent } from "./dishdetail/dishdetail.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./home/home.component";
+import { ContactComponent } from "./contact/contact.component";
+import { AboutComponent } from "./about/about.component";
 
 import { DishService } from "./services/dish.service";
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+
+import { AppRoutingModule } from "./app-routing/app-routing.module";
+
 @NgModule({
-  declarations: [AppComponent, MenuComponent, DishdetailComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    AppRoutingModule,
     MatToolbarModule,
     MatListModule,
     MatGridListModule,
