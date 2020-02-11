f222b0c38bd9d4cbe1e47a899afc83c5a27da1ae
VictorKanama <victor_kanama@hotmail.com>
2020-02-11 18:23:02 +0200

    Assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { DishService } from "./services/dish.service";
 import { PromotionService } from "./services/promotion.service";
 
 import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { LeaderService } from "./services/leader.service";
 
 @NgModule({
   declarations: [
@@ -45,7 +46,7 @@ import { AppRoutingModule } from "./app-routing/app-routing.module";
     MatCardModule,
     MatButtonModule
   ],
-  providers: [DishService, PromotionService],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
