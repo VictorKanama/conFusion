908cc513cc55d6a3c1f2e2a0fa6d8f6a180a7082
VictorKanama <victor_kanama@hotmail.com>
2020-02-12 19:06:21 +0200

    RxJs Part 1

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from "@angular/core";
 import { PROMOTIONS } from "../shared/promotions";
 import { Promotion } from "../shared/promotion";
 import { resolve } from "url";
+import { delay } from "rxjs/operators";
+import { of, Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root"
@@ -9,27 +11,19 @@ import { resolve } from "url";
 export class PromotionService {
   constructor() {}
 
-  getPromotions(): Promise<Promotion[]> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(PROMOTIONS), 2000);
-    });
+  getPromotions(): Observable<Promotion[]> {
+    return of(PROMOTIONS).pipe(delay(2000));
   }
 
-  getPromotion(id: string): Promise<Promotion> {
-    return new Promise(resolve => {
-      setTimeout(
-        () => resolve(PROMOTIONS.filter(promotion => promotion.id === id)[0]),
-        2000
-      );
-    });
+  getPromotion(id: string): Observable<Promotion> {
+    return of(PROMOTIONS.filter(promotion => promotion.id === id)[0]).pipe(
+      delay(2000)
+    );
   }
 
-  getFeaturedPromotion(): Promise<Promotion> {
-    return new Promise(resolve => {
-      setTimeout(
-        () => resolve(PROMOTIONS.filter(promotion => promotion.featured)[0]),
-        2000
-      );
-    });
+  getFeaturedPromotion(): Observable<Promotion> {
+    return of(PROMOTIONS.filter(promotion => promotion.featured)[0]).pipe(
+      delay(2000)
+    );
   }
 }
